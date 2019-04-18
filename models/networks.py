caf6c87652c90e9d607cc0c8e70a5cae160bc7f6
zhuliang <zhuliang@qiniu.com>
2019-04-18 11:27:22 +0800

    init

--- models/networks.py
+++ models/networks.py
@@ -127,12 +127,21 @@ class ResnetGenerator(nn.Module):
 
 		for i in range(n_downsampling):
 			mult = 2**(n_downsampling - i)
-			model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
-										 kernel_size=3, stride=2,
-										 padding=1, output_padding=1,
-										 bias=use_bias),
-					  norm_layer(int(ngf * mult / 2)),
-					  nn.ReLU(True)]
+			# model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+			# 							 kernel_size=3, stride=2,
+			# 							 padding=1, output_padding=1,
+			# 							 bias=use_bias),
+			# 		  norm_layer(int(ngf * mult / 2)),
+			# 		  nn.ReLU(True)]
+
+
+			model += [nn.UpsamplingBilinear2d(scale_factor=2),
+						nn.Conv2d(ngf * mult, int(ngf * mult / 2),
+                             kernel_size=3, stride=1,
+                             padding=1, bias=use_bias),
+                        norm_layer(int(ngf * mult / 2)),
+                        nn.ReLU(True)]
+
 		model += [nn.ReflectionPad2d(3)]
 		model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
 		model += [nn.Tanh()]
